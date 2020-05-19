1c9dda1fdb7e5fd7ac15c82631f0a49ff6ac4c33
Stevan Azlen <stevan.azlen@hpe.com>
2020-05-19 13:51:24 +0200

    End of section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -3,7 +3,7 @@ import { observable, action, computed, configure, runInAction } from "mobx";
 import { createContext, SyntheticEvent } from "react";
 import agent from "../api/Agent";
 
-configure({enforceActions: 'always' });
+configure({ enforceActions: "always" });
 
 class ActivityStore {
   @observable activityRegistry = new Map();
@@ -14,8 +14,23 @@ class ActivityStore {
   @observable target = "";
 
   @computed get activitiesByDate() {
-    return Array.from(this.activityRegistry.values()).sort(
+    return this.groupActivitiesByDate(
+      Array.from(this.activityRegistry.values())
+    );
+  }
+
+  groupActivitiesByDate(activities: IActivity[]) {
+    const sortedActivities = activities.sort(
       (a, b) => Date.parse(a.date) - Date.parse(b.date)
+    );
+    return Object.entries(
+      sortedActivities.reduce((activities, activity) => {
+        const date = activity.date.split("T")[0];
+        activities[date] = activities[date]
+          ? [...activities[date], activity]
+          : [activity];
+        return activities;
+      }, {} as { [key: string]: IActivity[] })
     );
   }
 
@@ -23,62 +38,63 @@ class ActivityStore {
     this.loadingInitial = true;
     try {
       const activities = await agent.Activities.list();
-      runInAction('loading activities', () => {
+      runInAction("loading activities", () => {
         activities.forEach((activity) => {
           activity.date = activity.date.split(".")[0];
           this.activityRegistry.set(activity.id, activity);
         });
         this.loadingInitial = false;
-      })
+      });
+      console.log(this.groupActivitiesByDate(activities));
     } catch (error) {
-      runInAction('display loading activities error', () => {
+      runInAction("display loading activities error", () => {
         this.loadingInitial = false;
-      })
+      });
       console.log(error);
     }
   };
 
   @action loadActivity = async (id: string) => {
     let activity = this.getActivity(id);
-    if (activity){
+    if (activity) {
       this.activity = activity;
     } else {
       this.loadingInitial = true;
       try {
         activity = await agent.Activities.details(id);
-        runInAction('getting activity', () =>{
+        runInAction("getting activity", () => {
           this.activity = activity;
           this.loadingInitial = false;
-        })
-      }catch (error){
-        runInAction('get activity error', () => {
+        });
+      } catch (error) {
+        runInAction("get activity error", () => {
           this.loadingInitial = false;
-        })
+        });
         console.log(error);
       }
     }
-  }
+  };
 
   @action clearActivity = () => {
     this.activity = null;
-  }
+  };
 
   getActivity = (id: string) => {
     return this.activityRegistry.get(id);
-  }
+  };
 
   @action createActivity = async (activity: IActivity) => {
     this.submitting = true;
     try {
       await agent.Activities.create(activity);
-      runInAction('creating activity', () =>{
+      runInAction("creating activity", () => {
         this.activityRegistry.set(activity.id, activity);
         this.submitting = false;
-      })
+      });
     } catch (error) {
-      runInAction('display creating activity error', () => {
+      runInAction("display creating activity error", () => {
         this.submitting = false;
-      })
+      });
       console.log(error);
     }
   };
@@ -87,15 +103,15 @@ class ActivityStore {
     this.submitting = true;
     try {
       await agent.Activities.update(activity);
-      runInAction('edit activity', () => {
+      runInAction("edit activity", () => {
         this.activityRegistry.set(activity.id, activity);
         this.activity = activity;
         this.submitting = false;
-      })
+      });
     } catch (error) {
-      runInAction('display edit activity error', () => {
+      runInAction("display edit activity error", () => {
         this.submitting = false;
-      })
+      });
       console.log(error);
     }
   };
@@ -108,16 +124,16 @@ class ActivityStore {
     this.target = event.currentTarget.name;
     try {
       await agent.Activities.delete(id);
-      runInAction('delete activity', () => {
+      runInAction("delete activity", () => {
         this.activityRegistry.delete(id);
         this.submitting = false;
         this.target = "";
-      })
+      });
     } catch (error) {
-      runInAction('delete activity error', () => {
+      runInAction("delete activity error", () => {
         this.submitting = false;
         this.target = "";
-      })
+      });
       console.log(error);
     }
   };
